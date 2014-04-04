ee914456290edec29f3d8094c4aed0ce7385ad7c
weston.mcbride <weston.mcbride@gmail.com>
2014-04-04 13:46:04 -0700

    Initial Commit

---
+++ NooraHealth/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NooraHealth
+//
+//  Created by WM on 4/4/14.
+//  Copyright (c) 2014 Noora Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([NHAppDelegate class]));
+	}
+}
